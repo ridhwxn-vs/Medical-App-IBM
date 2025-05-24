fe1fcc97e0bcde251ef2ec01cb8a6fa9df56bd3e
ridhwxn-vs <ridhwanvs04@gmail.com>
2025-05-24 09:36:51 -0400

    Dynamic App js editted

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+import Landing_page from './Components/Landing_Page/LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing_page />} />
+          {/* Add more routes here as needed */}
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
