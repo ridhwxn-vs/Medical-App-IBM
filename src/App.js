f5d5295e801545e0b1f902a431120119748db378
ridhwxn-vs <ridhwanvs04@gmail.com>
2025-05-24 12:01:41 -0400

    Consultation integrated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from './Components/Navbar/Navbar';
 import Landing_page from './Components/Landing_Page/LandingPage';
 import Login from './Components/Login/Login';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
-
+import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation';
 function App() {
   return (
     <>
@@ -14,6 +14,7 @@ function App() {
             <Route path="/" element={<Landing_page />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Sign_Up />} />
+            <Route path="/search/doctors" element={<InstantConsultation />} />
         </Routes>
       </BrowserRouter>
     </>
